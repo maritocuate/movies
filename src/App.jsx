7146ab0434e14652d94d297bfa5542962b165e1e
marioq <mario300@gmail.com>
2022-07-22 01:18:12 -0300

    chore: create Header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
-import { BrowserRouter as Router } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import { Header } from './ui/Header'
+import { Movies } from './ui/Movies'
+import { Home } from './ui/Home'
 import styles from './App.module.css'
 
 function App () {
   return (
+    <div className={styles.App}>
         <Router>
-            <div className={styles.App}>
-                <Header />
-            </div>
+            <Header />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/movies" element={<Movies />} />
+            </Routes>
         </Router>
+  </div>
   )
 }
 
--- src/ui/Header/index.jsx
+++ src/ui/Header/index.jsx
@@ -1,9 +1,46 @@
-import styles from './Header.module.css'
+import { Link, useLocation } from 'react-router-dom'
+import styles from './Header.module.scss'
 
 export function Header () {
+  const { pathname } = useLocation()
   return (
-        <header className={styles.header}>
-            <h1>Header</h1>
-        </header>
+        <nav className={styles.header}>
+            <Link to="/">
+                <img
+                    src="../images/logo.png"
+                    alt="Dreadful Tomato"
+                    className={styles.logo}
+                />
+            </Link>
+
+            {
+                (pathname === '/movies' || pathname === '/series')
+                  ? (
+                    <>
+                        <Link to="/movies">
+                            <div className={styles.navbutton}>
+                                <img
+                                    src="../images/icon-movies.png"
+                                    alt="icon movies"
+                                    className="button__icon"
+                                />
+                                <span>Movies</span>
+                            </div>
+                        </Link>
+                        <Link to="/series">
+                            <div className={styles.navbutton}>
+                                <img
+                                    src="../images/icon-series.png"
+                                    alt="icon movies"
+                                    className="button__icon"
+                                />
+                                <span>Series</span>
+                            </div>
+                        </Link>
+                    </>
+                    )
+                  : ('')
+              }
+        </nav>
   )
 }
