76ede36a6a86669a8ce336995667298a818a8495
marioq <mario300@gmail.com>
2022-07-22 00:05:11 -0300

    feat: setup eslint

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import { BrowserRouter as Router } from "react-router-dom";
-import { Header } from "./ui/Header";
-import styles from './App.module.css';
+import { BrowserRouter as Router } from 'react-router-dom'
+import { Header } from './ui/Header'
+import styles from './App.module.css'
 
-function App() {
-    return (
+function App () {
+  return (
         <Router>
             <div className={styles.App}>
                 <Header />
             </div>
         </Router>
-    )
+  )
 }
 
-export default App;
+export default App
--- src/ui/Movies/index.jsx
+++ src/ui/Movies/index.jsx
@@ -1,12 +1,9 @@
 import styles from './Movies.module.css'
 
-export function Movies() {
-
-    return (
+export function Movies () {
+  return (
         <div className={styles.movies}>
             <h1>Movies</h1>
         </div>
-    )
-
-
+  )
 }
