28819abf5fca0e525f23b4d05a999f3716a5f909
marioq <mario300@gmail.com>
2022-07-22 01:56:45 -0300

    chore: create Home component

--- src/ui/Home/index.jsx
+++ src/ui/Home/index.jsx
@@ -1,9 +1,29 @@
-import styles from './Home.module.css'
+import { Link } from 'react-router-dom'
+import './styles.scss'
 
 export function Home () {
   return (
-        <div className={styles.home}>
-            <h1>Home</h1>
+        <div className='home'>
+            <Link to="/movies">
+              <section className='section'>
+                <img
+                  src="../images/movies.png"
+                  alt="movies"
+                  className="section__img"
+                />
+                <h2 className="section__tittle section__tittle--opacity">Movies</h2>
+              </section>
+            </Link>
+            <Link to="/series">
+              <section className='section'>
+                <img
+                  src="../images/series.png"
+                  alt="series"
+                  className="section__img"
+                />
+                <h2 className="section__tittle">Series</h2>
+              </section>
+            </Link>
         </div>
   )
 }
